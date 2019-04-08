9fadea1d6cbf34118e726e805f0553b5bd3b6695
Elisa Stranzinger <elisa.stranzinger@htl-salzburg.ac.at>
2019-04-09 00:00:29 +0200

    added unity file with non working css file containing operators

---
+++ Assets/Scripts/ProgBasics.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("erste Tests mit Operatoren");
+        Debug.Log("Addirion" + (5+2));
+        Debug.Log("Multiplikation" + (6*12));
+        Debug.Log("Subtraktion" + (124050-10250));
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
