0c1785a140f2466dea571e3441936d373eb633b2
Elisa Stranzinger <elisa.stranzinger@htl-salzburg.ac.at>
2019-04-09 09:59:57 +0200

    fixed c# script, is now working in the project

---
+++ Assets/MyGame/Scripts/ProgBasics.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgBasics : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("erste Tests mit Operatoren");
+        Debug.Log("Addition " + (5+2));
+        Debug.Log("Multiplikation " + (6*12));
+        Debug.Log("Subtraktion " + (124050-10250));
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
