e17df4e7d73de5bd2ba1fb556144d8874dd48dfe
Elisa Stranzinger <elisa.stranzinger@htl-salzburg.ac.at>
2019-04-10 13:41:01 +0200

    updated c# file - methods

--- Assets/MyGame/Scripts/ProgBasics.cs
+++ Assets/MyGame/Scripts/ProgBasics.cs
@@ -7,14 +7,38 @@ public class ProgBasics : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         Debug.Log("erste Tests mit Operatoren");
+
         Debug.Log("Addition " + (5+2));
         Debug.Log("Multiplikation " + (6*12));
         Debug.Log("Subtraktion " + (124050-10250));
 
+        Operators();
+        XY();
+        Nose();
+        Count();
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    public void Operators()
+    {
+        Debug.Log("Test");
+    }
+
+    public void XY()
+    {
+        Debug.Log("something");
+    }
+
+    void Nose()
+    {
+        Debug.Log("Leg");
+    }
+    private void Count()
+    {
+        Debug.Log((8+6-4+26-12));
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
