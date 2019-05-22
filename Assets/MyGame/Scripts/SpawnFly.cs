cb9cf2cd6cdef948832f8e867266a155c6d7d226
Elisa Stranzinger <elisa.stranzinger@htl-salzburg.ac.at>
2019-05-22 13:04:56 +0200

    collecting collectable test scene and script

---
+++ Assets/MyGame/Scripts/SpawnFly.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFly : MonoBehaviour 
+{
+    public Fly flyPrefab;
+
+    private void Update()
+    {
+        SpawnFlyy();
+    }
+
+    private void SpawnFlyy()
+    {
+        Fly flyClone = (Fly)Instantiate(flyPrefab, transform.position, transform.rotation);
+        flyClone.transform.SetParent(flyParent.transform);
+        flyClone.transform.localPosition = new Vector3(Random.Range(xMinLeft, xMaxLeft), flyParent.transform.position.y, 0f);
+    }
+}
